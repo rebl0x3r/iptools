07ed803e1e37a8d9cfc4bb17db64ef5b3bec9487
mrblackx <63623649+rebl0x3r@users.noreply.github.com>
2021-03-24 06:20:21 +0100

    Update ip-domain.py

--- ip-domain.py
+++ ip-domain.py
@@ -3,7 +3,7 @@
 
 
 
-import socket,os,sys
+import socket,os,sys,threading,queue,time,random
 from datetime import date
 
 d = []
@@ -14,10 +14,13 @@ title = "IP-2-DOMAIN | {} | @MrBlackX".format(dl)
 os.system('echo -n -e "\033]0;{}\007"'.format(title))
 os.system('clear')
 
+num = 0
+
+
 print("""
 \033[37;1m
   ___ ____ ____  ____                        _       
- |_ _|  _ \___ \|  _ \  ___  _ __ ___   __ _(_)_ __\033[31mV2\033[37m
+ |_ _|  _ \___ \|  _ \  ___  _ __ ___   __ _(_)_ __\033[31mV3\033[37m
   | || |_) |__) | | | |/ _ \| '_ ` _ \ / _` | | '_ \ 
   | ||  __// __/| |_| | (_) | | | | | | (_| | | | | |
  |___|_|  |_____|____/ \___/|_| |_| |_|\__,_|_|_| |_|
@@ -27,34 +30,54 @@ print("""
 """)
 
 
-def filecheck():
-	if os.path.isfile("Domains.txt"):
-		os.rename("Domains.txt", "Domains-Old.txt")
+class Checker:
+  inputQueue = queue.Queue()
 
-  
+  def __init__(self):
+    self.leads = input("\033[34;1m[IP-2-DOMAIN] \033[32;1mIPs:\033[0m\033[37;1m ")
+    self.threads = input("\033[34;1m[IP-2-DOMAIN] \033[32;1mHow many threads?:\033[0m\033[37;1m ") 
+    self.length = len(list(open(self.leads)))
+    print('')
 
-def iptodomain(file):
-  t = open(file)
-  filecheck()
-  l = len(t.readlines())
-  t.close()
-  t = open(file)
-  num = 1
-  for Hostname in t:
-    Hostname = Hostname.rstrip()
+  def save_to_fie(self, sent, lead):
+      kl = open(sent, "a+")
+      kl.write(lead)
+      kl.close()
+
+  def send(self,target):
+    ip = target
     try:
-      num += 1
-      s = socket.gethostbyaddr(Hostname)[0]
-      d.append(s)
-      print(f"\033[34m[\033[37m{num}\033[34m|\033[37m{l}\033[34m] \033[32m{Hostname}  \033[32m[Valid] ===> https://{s}\033[0m")
-      dd=open("Domains.txt","a+")
-      dd.write(s+"\n")
-      dd.close()
+      s = socket.gethostbyaddr(ip)[0]
+      return s
     except socket.herror:
-      print(f"\033[34m[\033[37m{num}\033[34m|\033[37m{l}\033[34m] \033[31m{Hostname} \033[31m[Not Valid]\033[0m")
-      continue
+      #pass
+      return 'dead'
+  
+  def check(self):
+    global num
+    global l
+    while 1:
+      target = self.inputQueue.get()
+      num += 1
+      result = self.send(target)
+      if result == "dead":
+        print(f"\033[34m[\033[37m{num}\033[34m|\033[37m{self.length}\033[34m] \033[31m{target} \033[31m[Not Valid]\033[0m")
 
+      else:
+        print(f"\033[34m[\033[37m{num}\033[34m|\033[37m{self.length}\033[34m] \033[32m{target}  \033[32m[Valid] ===> https://{result}\033[0m")
+        kl = open("Domains.txt", "a+")
+        kl.write(result+"\n")
+        kl.close()
 
-f = str(input('\033[34mEnter the file you want to extract: '))
-print("\033[0m")
-iptodomain(f)
+  def run_thread(self):
+    while True:
+      for x in range(int(self.threads)):
+        t = threading.Thread(target=self.check)
+        t.setDaemon(True)
+        t.start()
+      for y in open(self.leads, 'r').readlines():
+        self.inputQueue.put(y.strip())
+      self.inputQueue.join()
+
+start = Checker()
+start.run_thread()
